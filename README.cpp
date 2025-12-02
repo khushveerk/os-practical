f380efe3866fb271fbd812bcdef407472ab55dfe
khushveerk <khushveer.prof011@gmail.com>
2025-12-02 20:46:14 +0530

    Update README with practical programming tasks

---
+++ README.cpp
@@ -0,0 +1,68 @@
+/*# os-practical
+
+## in every practical cmds used - 
+1-  nano filename.cpp  --- Press Ctrl + O → then hit -- Enter -- Then press Ctrl + X → this eXits nano.
+2-  cat filename.cpp 
+3-  g++ filename.cpp -o filename
+4-  ./filename
+
+
+
+practical 2 -ii.Process Control: fork, getpid, ps, kill, sleep. 
+ file name - fork.cpp */
+
+
+
+/* 3. Write a programme (using fork() and/or exec() commands) where parent 
+ same as previous */
+
+
+/* 3.iii. Before terminating, the parent waits for the child to finish its task.
+  file name - parentwait.cpp */ 
+
+
+/* 4. Write a program to report behaviour of Linux kernel including kernel
+version, CPU type and model. (CPU informa on) 
+  file name - report.cpp */ 
+
+
+/* 5. Write a program to report behaviour of Linux kernel including configured
+memory, amount of free and used memory. (Memory informa on)
+  file name - memory.cpp*/
+
+
+/* 6. write a program to copy files using system calls.
+  file name - cpoy_sytemcalls.cpp */
+
+
+/* 7. Write a program to implement first-come, first-served FCFS scheduling
+algorithm in CPP code.
+  fule name - fcfs.cpp */ 
+
+
+/* 8. Write a program to implement Shortest Job First SJF Scheduling Algorithm.
+      file name - sjf.cpp */
+
+/* 9. Write a program to implement non-primitive priority-based scheduling
+algorithm.
+  file name - priority.cpp */ 
+
+
+/* 10. Write a program to implement SRTF scheduling algorithm.
+   file name - srtf.cpp */ 
+
+/* 11. Write a program to calculate sum of n numbers using pthreads. A list of n
+numbers is divided into two smaller lists of equal size. Two separate threads
+are used to run the sublist.
+
+ file name - pthreads.cpp */
+
+
+/* 12. write a program to implement first-fit, best-fit and worst-fit alloca on
+strategies.
+  file name - allocation.cpp */ 
+
+
+
+
+
